f9bb3651ee345a6eaa77a0102cde8c6f4623fe33
Kashish Thesia <kthesia93@gmail.com>
2025-07-03 14:47:29 +0200

    Fix: Added ReactDOM render entry point

--- index.jsx
+++ index.jsx
@@ -1,17 +1,7 @@
 import React from "react";
-import AlumniBrowser from "./components/AlumniBrowser";
-import IntroRequestForm from "./components/IntroRequestForm";
-import CommunityAMA from "./components/CommunityAMA";
-import NetworkAtCompany from "./components/NetworkAtCompany";
+import ReactDOM from "react-dom/client";
+import AlumniMentorNetworkHub from "./AlumniMentorNetworkHub";
+import "./index.css";
 
-export default function AlumniMentorNetworkHub() {
-  return (
-    <div className="max-w-5xl mx-auto p-6 space-y-6">
-      <h1 className="text-2xl font-bold mb-4">👥 Alumni & Mentor Network Hub</h1>
-      <AlumniBrowser />
-      <IntroRequestForm />
-      <CommunityAMA />
-      <NetworkAtCompany />
-    </div>
-  );
-}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AlumniMentorNetworkHub />);
